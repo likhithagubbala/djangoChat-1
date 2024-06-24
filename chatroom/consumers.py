42b2ecec5c15b97347548b5876766da4fa84f445
Reshma <120391165+Reshmareshu786@users.noreply.github.com>
2024-06-24 15:30:03 +0530

    redesigned template

--- chatroom/consumers.py
+++ chatroom/consumers.py
@@ -6,62 +6,44 @@ from .models import Room, Message
 import re
 
 class ChatConsumer(AsyncWebsocketConsumer):
-    #connect method
     async def connect(self):
-        # Get room name from URL kwargs
         room_name = self.scope['url_route']['kwargs']['room_name']
-        
-        # Sanitize room name
-        sanitized_room_name = re.sub(r'\W+', '', room_name)  # Remove non-alphanumeric characters
+        sanitized_room_name = re.sub(r'\W+', '', room_name)
         self.room_name = f"room_{sanitized_room_name}"
         
-        # Check if sanitized room name is empty
         if not self.room_name:
-            # Handle invalid room name
             await self.close()
 
-        # Add channel to the group
         await self.channel_layer.group_add(self.room_name, self.channel_name)
-
-        # Accept incoming WebSocket connection
         await self.accept()
 
-    #disconnecting
     async def disconnect(self, code):
-        # Remove channel from the group
         await self.channel_layer.group_discard(self.room_name, self.channel_name)
         await super().disconnect(code)
 
-    #recieve messages
     async def receive(self, text_data):
         data_json = json.loads(text_data)
-        #print(data_json)
-    # Example: Echo the received message back to the client
-        await self.send(text_data=json.dumps(data_json))
         event = {
             "type": "send_message",
             "message": data_json
         }
 
-        await self.channel_layer.group_send(self.room_name,event)
+        await self.channel_layer.group_send(self.room_name, event)
 
-    #creating message to database
-    async def send_message(self,event):
+    async def send_message(self, event):
         data = event["message"]
-        await self.create_message(data = data)
+        await self.create_message(data)
 
         response = {
-            "sender"  : data["sender"],
+            "sender": data["sender"],
             "message": data["message"]
         }
 
-        await self.send(text_data = json.dumps({"message":response}))
+        await self.send(text_data=json.dumps({"message": response}))
 
     @database_sync_to_async
-    def create_message(self,data):
+    def create_message(self, data):
         get_room = Room.objects.get(room_name=data['room_name'])
         
-        if not Message.objects.filter(message=data['message'],sender=data["sender"]).exists():
-            new_message = Message.objects.create(room = get_room,message=data['message'],sender=data["sender"])
-
-    
+        if not Message.objects.filter(message=data['message'], sender=data["sender"]).exists():
+            Message.objects.create(room=get_room, message=data['message'], sender=data["sender"])
--- chatroom/views.py
+++ chatroom/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Message
 
+@login_required
 def HomeView(request):
     if request.method == "POST":
         username = request.POST["username"]
@@ -17,6 +21,7 @@ def HomeView(request):
     }
     return render(request, 'home.html', context)
 
+@login_required
 def RoomView(request, room_name, username):
     try:
         existing_room = Room.objects.get(room_name__iexact=room_name)
@@ -30,3 +35,29 @@ def RoomView(request, room_name, username):
         "room_name": existing_room.room_name
     }
     return render(request, 'room.html', context)
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid credentials'})
+    return render(request, 'login.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
